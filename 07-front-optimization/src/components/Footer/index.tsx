d4a3edee0f558aad520e19b040f1da7babd05cd4
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-19 11:20:29 +0900

    feat : Article

---
+++ 07-front-optimization/src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+export default function Footer() {
+  return (
+    <div className={"Footer"}>©Performance Optimization Lecture for React</div>
+  );
+}
---
+++ 07-front-optimization/src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./index.css";
+
+export default function Index() {
+  return <div className={"Header"}>Lecture #1</div>;
+}
