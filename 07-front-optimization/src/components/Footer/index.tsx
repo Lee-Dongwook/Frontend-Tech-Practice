c88f895b170551083c7efb4c6f3322b71563cac0
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-19 11:45:26 +0900

    remove

--- 07-front-optimization/src/components/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-export default function Footer() {
-  return (
-    <div className={"Footer"}>©Performance Optimization Lecture for React</div>
-  );
-}
